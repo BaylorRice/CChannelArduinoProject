790b1f32ce56a4d0d05cdb4a201337726bfc8eb8
Jonathan <jonathanbonilla2012@gmail.com>
2023-10-24 15:58:42 -0500

    Update setup.h

--- include/setup.h
+++ include/setup.h
@@ -6,11 +6,12 @@
 #include <Stepper.h>
 #include <Servo.h>
 
-// Define component pins
-// TO DO: Update Button pins
+//Define component pins
 // Buttons
+// TO DO: Update Button pins
 #define GREEN_START_BTN_PIN 9999
 #define GOLD_START_BTN_PIN 9998
+
 // Ultrasonic Sensors
 #define TRIG_PIN_GREEN 52
 #define ECHO_PIN_GREEN 53
@@ -18,12 +19,15 @@
 #define ECHO_PIN_GOLD 51
 #define TRIG_PIN_PLL 48
 #define ECHO_PIN_PLL 49
+// SENSOR CONFIG
 #define MAX_DISTANCE 400
 #define NUM_PINGS 15
+
 // DC Motor
 #define Y_DC_IN1 22
 #define Y_DC_IN2 23
 #define Y_DC_EN 2
+
 // Stepper motors
 #define X_STEP_IN1 33
 #define X_STEP_IN2 35
@@ -33,14 +37,22 @@
 #define Z_STEP_IN2 36
 #define Z_STEP_IN3 34
 #define Z_STEP_IN4 32
+// STEPPER CONFIG
 #define STEPS_PER_REVOLUTION 200
 #define SPEED 200
+
 // Servo Motors
 // TO DO: Update servo pins
 #define SERVO_GRAB_PIN 9997
 #define SERVO_LIFT_PIN 9996
+// SERVO CONFIG
 int servo_g_pos = 0;
 int servo_l_pos = 0;
+
+// Limit Switchs
+// TO DO: Update limit switch pins
+#define LIMIT_SWITCH_1_PIN 9995
+#define LIMIT_SWITCH_2_PIN 9994
 
 // Button setup
 using Button = AblePulldownCallbackClickerButton;
@@ -50,16 +62,16 @@ Button goldStart(GOLD_START_BTN_PIN);
 Button *btns[] = {&greenStart, &goldStart};
 ButtonList btnList(btns);
 
-// Initialize sensors
+// Sensor Setup
 NewPing sonarGreen(TRIG_PIN_GREEN, ECHO_PIN_GREEN, MAX_DISTANCE);
 NewPing sonarGold(TRIG_PIN_GOLD, ECHO_PIN_GOLD, MAX_DISTANCE);
 NewPing sonarPLL(TRIG_PIN_PLL, ECHO_PIN_PLL, MAX_DISTANCE);
 
-// Initialize Steppers
+// Stepper setup
 Stepper xStep(STEPS_PER_REVOLUTION, X_STEP_IN1, X_STEP_IN2, X_STEP_IN3, X_STEP_IN4);
 Stepper zStep(STEPS_PER_REVOLUTION, Z_STEP_IN1, Z_STEP_IN2, Z_STEP_IN3, Z_STEP_IN4);
 
-// Initialize Servos
+// Servo setup
 Servo gServo;
 Servo lServo;
 
