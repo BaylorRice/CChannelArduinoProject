3aa258cba1664d6389afe63fd1150efcffc9b3f1
Reese Ford <87844576+BaylorRice@users.noreply.github.com>
2023-10-24 15:11:14 -0500

    Update movement.h

--- include/movement.h
+++ include/movement.h
@@ -8,18 +8,24 @@ class location {
     double zPos;
     double zRot;
 
-    public:
-    location(double xIn = 0, double yIn = 0, double zIn = 0, double zRotIn = 0);
-    
     void setXPos(double xIn);
     void setYPos(double yIn);
     void setZPos(double zIn);
     void setZRot(double zRotIn);
 
+    
+    public:
+    location(double xIn = 0, double yIn = 0, double zIn = 0, double zRotIn = 0);
+
     const double getXPos();
     const double getYPos();
     const double getZPos();
     const double getZRot();
+
+    void moveX(double xIn);
+    void moveY(double yIn);
+    void moveZ(double zIn);
+    void rotateZ(double zRotIn);
 };
 
 #endif //MOVEMENT_H
