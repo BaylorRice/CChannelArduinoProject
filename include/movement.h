90a26ef13320eac80fc252425ab2831b80a55cc6
Reese Ford <87844576+BaylorRice@users.noreply.github.com>
2023-10-24 16:30:17 -0500

    Added Direction and Speed Check

--- include/movement.h
+++ include/movement.h
@@ -22,7 +22,7 @@ class Location {
     const double getZRot();
 
     void moveX(double xIn);
-    void moveYfor(double time, int speed);
+    void moveYfor(double time, int speed, int dir);
     void moveZ(double zIn);
     void rotateZ(double zRotIn);
 };
