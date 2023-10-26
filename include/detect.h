ad73a273c2b84cfa6b4c4724f606b3ae57a88681
samuelsimonian <148909688+samuelsimonian@users.noreply.github.com>
2023-10-26 13:58:26 -0500

    detect.cpp

---
+++ include/detect.h
@@ -0,0 +1,17 @@
+#ifndef CCHANNELARUDINOPROJECT_DETECT_H
+#define CCHANNELARUDINOPROJECT_DETECT_H
+
+#include "setup.h"
+
+class detect {
+private:
+    bool goldReady;
+    bool greenReady;
+    bool palletReady;
+    bool green;
+    bool gold;
+
+};
+
+
+#endif //CCHANNELARUDINOPROJECT_DETECT_H
