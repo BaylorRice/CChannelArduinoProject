b7d527e81f10a2043b0ef6d2be7c53283ec51d4a
samuelsimonian <148909688+samuelsimonian@users.noreply.github.com>
2023-11-02 13:38:39 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -343,33 +343,34 @@ void loop() {
   // Looping Code goes here
 }
 
-/*
-Loop Code from Button Testing
+
+//Loop Code from Button Testing
 
 // put your main code here, to run repeatedly:
-  btnList.handle();
-  detect data;
-  NewPing selection(0,0,0);
-  bool i = true;
+void main() {
+    btnList.handle();
+    detect data;
+    NewPing selection(0, 0, 0);
+    bool i = true;
 
-  Serial.println("Start");
+    Serial.println("Start");
 
-  while (i) {
-    if(greenStart.resetClicked()) {
-      Serial.print("Green\n");
-      i = false;
+    while (i) {
+        if (greenStart.resetClicked()) {
+            Serial.print("Green\n");
+            i = false;
+        }
+        if (goldStart.resetClicked()) {
+            Serial.print("Gold\n");
+            i = false;
+        }
+
     }
-    if (goldStart.resetClicked()) {
-      Serial.print("Gold\n");
-      i = false;
-    }
-
-  }
-  delay(1000);
+    delay(1000);
+}
 
 
 
   Serial.println("\nStop");
   delay(1000);
 
-  */
