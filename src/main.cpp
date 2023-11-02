d5fe642e3c0117273cb81c029a1445ab6727a454
Reese Ford <87844576+BaylorRice@users.noreply.github.com>
2023-11-02 14:58:36 -0500

    Removed button testing loop code comments

--- src/main.cpp
+++ src/main.cpp
@@ -343,34 +343,3 @@ Claw claw;
 void loop() {
   // Looping Code goes here
 }
-
-/*
-Loop Code from Button Testing
-
-// put your main code here, to run repeatedly:
-  btnList.handle();
-  detect data;
-  NewPing selection(0,0,0);
-  bool i = true;
-
-  Serial.println("Start");
-
-  while (i) {
-    if(greenStart.resetClicked()) {
-      Serial.print("Green\n");
-      i = false;
-    }
-    if (goldStart.resetClicked()) {
-      Serial.print("Gold\n");
-      i = false;
-    }
-
-  }
-  delay(1000);
-
-
-
-  Serial.println("\nStop");
-  delay(1000);
-
-  */
